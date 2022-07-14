421cfc7f9bdf105758c0b4d8e7d9d4b1c713ccd0
Siddhartha <siddharthchauhan888@gmail.com>
2022-07-14 23:26:46 +0530

    Added UserDetails service

---
+++ SpringCRUDandSecurity/src/main/java/com/student/SpringCRUDandSecurity/service/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.student.SpringCRUDandSecurity.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.student.SpringCRUDandSecurity.entity.User;
+import com.student.SpringCRUDandSecurity.repository.UserRepository;
+import com.student.SpringCRUDandSecurity.security.MyUserDetails;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	   @Autowired
+	    private UserRepository userRepository;
+	     
+	    @Override
+	    public UserDetails loadUserByUsername(String username)
+	            throws UsernameNotFoundException {
+	        User user = userRepository.getUserByUsername(username);
+	         
+	        if (user == null) {
+	            throw new UsernameNotFoundException("Could not find user");
+	        }
+	         
+	        return new MyUserDetails(user);
+	    }
+
+}
